29c7b069eea4d430646a82fbade619a31ab38551
JONI <77753643+joniharadinajj@users.noreply.github.com>
2024-07-16 10:45:37 +0200

    Add files via upload

---
+++ index4.py
@@ -0,0 +1,7 @@
+country = ["Kosova","Germany","Switzerland","Albania","USA"]
+print(country)
+country.sort()
+country.append("Italy")
+country.remove("Switzerland")
+print(country)
+print(country[0])
